e73ef572c26bcebee7903ada735b1f8e734f9c0e
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-02-19 21:25:31 +0800

    added admin panel for Recipe

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+admin.site.register(Recipe, RecipeAdmin)
