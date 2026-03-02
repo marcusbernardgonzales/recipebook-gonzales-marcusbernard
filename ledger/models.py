e96bd55ffdbba053001caf05d351db50cf030b89
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-02 18:13:02 +0800

    updated Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -20,6 +20,15 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="recipe_author",
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
     
