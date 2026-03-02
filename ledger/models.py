38bbf4d2569991e2daea05aa6e3cb72a28174c0c
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-02 18:05:47 +0800

    added Profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(validators=[MinLengthValidator(255)])
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +16,7 @@ class Ingredient(models.Model):
     
     def get_absolute_url(self):
         return reverse('ingredient', args=[self.pk])
+    
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
