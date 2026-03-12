a15e9ae6e131ca6e90c1f1821b0db74102c9fc3a
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-12 16:20:17 +0800

    modified Recipe admin to contain RecipeImage models

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeImageInline, ]
 
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 admin.site.register(Recipe, RecipeAdmin)
+
+
