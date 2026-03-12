35c80deebe19f1ad8c67382afbbe9a1aaa0f1162
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-12 20:25:31 +0800

    added RecipeImageForm model

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
+
+
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ['image', 'description', ]
